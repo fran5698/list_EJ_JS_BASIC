7c3ec185ff5cb07d134cb55d271da7f0a69ce97b
fran5698 <146084710+fran5698@users.noreply.github.com>
2023-09-28 12:24:48 +0200

    Add files via upload

---
+++ lista JS II/ej2/list JS basic II ej 2.js
@@ -0,0 +1,12 @@
+
+function init(n1) {
+    /*
+    if (n1 % 2 === 0) {
+        alert(n1 + " es par");
+    } else {
+        alert(n1 + " es impar");
+    }
+    */
+    // operador ternario
+    n1 % 2 === 0 ? alert(n1 + " es par") : alert(n1 + " es impar");
+}
